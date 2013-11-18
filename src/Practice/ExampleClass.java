037b290391cc6aadb68480c4382a7a00323b7a79
Juan Carlos Espinoza <jcespinoza@unitec.edu>
2013-11-18 06:30:12 -0600

    Initial Commit

---
+++ src/Practice/ExampleClass.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Practice;
+
+import org.opensourcephysics.display2d.*;
+/**
+ * @author Juan Carlos Espinoza
+ *
+ */
+public class ExampleClass {
+
+}
